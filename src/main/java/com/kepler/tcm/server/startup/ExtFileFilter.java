932f348d2021b38ec1457cff25d7804299696417
014621 <wangshuping@bonc.com.cn>
2018-06-06 14:54:57 +0800

    startup

---
+++ src/main/java/com/kepler/tcm/server/startup/ExtFileFilter.java
@@ -0,0 +1,28 @@
+package com.kepler.tcm.server.startup;
+
+import java.io.File;
+import java.io.FilenameFilter;
+/**
+ * 文件过滤器
+ * @author wangsp
+ * @date 2018年6月6日
+ * @version V1.0
+ */
+public class ExtFileFilter implements FilenameFilter {
+
+	private String strExtendName = "";
+
+	public ExtFileFilter(String extendName) {
+		this.strExtendName = extendName;
+	}
+
+	public boolean accept(File dir, String name) {
+		if ((new File(dir.getPath() + "/" + name).isDirectory())
+				|| (name.toLowerCase().endsWith(this.strExtendName
+						.toLowerCase()))) {
+			return true;
+		}
+
+		return false;
+	}
+}

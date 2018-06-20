d657ba3679c74424bf115c0fc3e64d2b106e8807
Your Name <you@example.com>
2018-06-20 18:04:23 +0800

    数据库配置  任务管理页面

---
+++ src/main/resources/public/pages/js/databaseConfig.vue.js
@@ -0,0 +1,122 @@
+$(function() {
+
+    $(".pagleft input").on("change", function() {
+        this.value = this.value.replace(/[^0-9]+/, '');
+        if(this.value <= 0) {
+            this.value = 1;
+        }
+        init(1);
+    });
+
+    $(".required").hover(function() {
+		if($(this).val() == '') {
+			$(this).popover('show');
+		}
+	}, function() {
+		$(this).popover('hide');
+	});
+    
+	init(1);
+});
+
+//表格数据及分页
+var vm = new Vue({
+	el: "#app",
+	data: {
+		tabData: [],
+		current: 1, //当前显示第几页
+		showItem: 5,
+		allpage: 13,
+		totalNum: 0,
+		checkAll:false,
+		checkModel:[]
+		
+	},
+	computed: {
+		pages: function() {
+			var pag = [];
+			if(this.current < this.showItem) { //如果当前的激活的项 小于要显示的条数
+				//总页数和要显示的条数那个大就显示多少条
+				var i = Math.min(this.showItem, this.allpage);
+				while(i) {
+					pag.unshift(i--);
+				}
+			} else { //当前页数大于显示页数了
+				var middle = this.current - Math.floor(this.showItem / 2), //从哪里开始
+					i = this.showItem;
+				if(middle > (this.allpage - this.showItem)) {
+					middle = (this.allpage - this.showItem) + 1
+				}
+				while(i--) {
+					pag.push(middle++);
+				}
+			}
+			return pag;
+		}
+	},
+	/*methods: {
+		goto: function(index) {
+			//if(index == this.current) return;
+			this.current = index;
+
+			// 取消全选
+			if($("#app").find("thead input").is(':checked')){
+				$("#app").find("thead input").attr("checked",false);
+				$("#app").find("thead input").parent().removeClass("active");
+			}
+			// 取消tr选中的背景状态
+			if($("#app").find("tbody>tr").hasClass("active"))$("#app").find("tbody>tr").removeClass("active");
+			
+			//这里可以发送ajax请求
+			init(index);
+		},
+        //修改
+        Updatask: function() {
+        	
+        },
+        //删除
+        tabDelete: function(id,isDeleted) {
+			$(".delete-modal .modal-title").text('是否删除？');
+			//确定删除
+			$(".delete-modal button[type=submit]").click(function() {
+				$(".loading").show();
+				$.ajax({
+					type: "put",
+					url: util.agent().baseUrl + "/custom/update.json",
+					data: {"isDeleted" : 2,"id" : id},
+					async: true,
+					success: function() {
+						layer.msg("删除成功", {
+							time: 1000
+						});
+
+						init(1);
+					}
+				});
+			});
+
+		},
+	}*/
+});
+
+function init(index) {
+	var params = {};
+	//console.log("http://"+util.agent().baseUrl+"/git/kepler-tcm/src/main/resources/public/pages/js/data.json");
+	$.ajax({
+		type: "get",
+		url: "http://"+util.agent().baseUrl+"/git/kepler-tcm/src/main/resources/public/pages/js/data.json",
+		async: true,
+		data: params,
+		success: function(data) {
+			if(data.length>0&&data!=undefined){
+        		vm.current = index;
+        		$("#app .no-data").remove();
+        	}else{
+        		vm.current = 0;
+        		$("#app table>tbody").empty();
+        		$("#app .no-data").remove();
+        		$("#app .pagleft").before('<div class="no-data"><p></p><h4>暂时没有数据</h4></div>');
+     	    }
+		}
+	});
+}
